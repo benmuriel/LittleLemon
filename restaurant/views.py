8de735db310d825750477c2f6010427b1c896e30
ben.muriel <ben.muriel@outlook.com>
2025-07-11 16:08:11 +0200

    Booking VewSet  API

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,6 +1,34 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework import viewsets
+from rest_framework import permissions
+from rest_framework.decorators import api_view
+from .models import Menu, Booking
+from django.contrib.auth.models import User
+from .serializers import UserSerializer, MenuItemSerializer, BookingSerializer
 
-# Create your views here.
+# Create your views here. 
+
+
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuItemSerializer
+
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = Menu.objects.all()
+    serializer_class = MenuItemSerializer
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 
 def index(request):
